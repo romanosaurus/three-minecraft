06bb6357d69c1ad7f3d25a7520b841127f227f70
charlie <charlie@localhost.localdomain>
2020-01-09 14:17:57 +0100

    + Add basic movement and first person view for steve

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import Voxel from "./components/utils/Voxel";
 import LightningUtilities from "./components/lights/LightningUtilities";
 import ModelLoader from "./components/models/ModelLoader";
 import Object3 from "./components/models/Object3";
+import { MOUSE } from "three";
 
 class Window
 {
@@ -12,7 +13,6 @@ class Window
     private renderer: THREE.WebGLRenderer;
     private windowNeedToResize: boolean;
     private readonly mainScene: THREE.Scene;
-    private controllers: OrbitControls;
     private modelLoader: ModelLoader;
 
     private steve: Object3;
@@ -24,33 +24,58 @@ class Window
         this.windowNeedToResize = false;
         this.camera = new THREE.PerspectiveCamera(75, 2, 0.1, 1000);
         this.mainScene = new THREE.Scene();
-        this.controllers = new OrbitControls( this.camera, this.renderer.domElement );
 
         this.steve =  new Object3("Steve", "./assets/steve/minecraft-steve.obj", "./assets/steve/minecraft-steve.mtl");
+        this.steve.getObject().then(object => {
+            object.position.x = 50;
+            object.position.z = 40;
+            object.position.y = 20;
+        });
     }
 
     private onDocumentKeyDown(e)
     {
+        // init basic speed x and y
+        var xSpeed = 1;
+        var ySpeed = 1;
+   
+        // Update steve and camera position
         this.steve.getObject().then(object => {
-            switch(e.key) {
-                case "s":
-                    console.log('lel')
-                    object.position.x -= 10;
-                    break;
-                case "z":
-                    object.position.x += 10;
-                    break;
-                case "q":
-                    object.position.z += 10;
-                    break;
-                case "d":
-                    object.position.z -= 10;
-                    break;
-            }
-            object.position.x += 10;
+            if (e.key == "s")
+                object.position.x -= xSpeed;                
+            if (e.key == "z")
+                object.position.x += xSpeed;
+            if (e.key == "q")
+                object.position.z -= ySpeed;
+            if (e.key == "d")
+                object.position.z += ySpeed;
+            this.camera.position.set(object.position.x + 1, object.position.y + 8, object.position.z);
         });
+    }
 
-        console.log(e);
+
+    private CameraMovement(eventMouse)
+    {
+        const euler = new THREE.Euler( 0, 0, 0, 'YXZ' );
+        var PI_2 = Math.PI / 2;
+        const movementX = eventMouse.movementX || eventMouse.mozMovementX || eventMouse.webkitMovementX || 0;
+        const movementY = eventMouse.movementY || eventMouse.mozMovementY || eventMouse.webkitMovementY || 0;
+
+        euler.setFromQuaternion( this.camera.quaternion );
+
+		euler.y -= movementX * 0.005;
+		euler.x -= movementY * 0.005;
+
+        euler.x = Math.max( - PI_2, Math.min( PI_2, euler.x ) );
+       
+        // fix limit for camera vision
+        if (euler.x < -1 || euler.x > 1 || euler.y < -3)
+            return;
+        else if (euler.y < -2.5 || euler.y > - 0.50)
+            return;
+        else {
+            this.camera.quaternion.setFromEuler(euler);
+        }
     }
 
     private InitScene()
@@ -60,7 +85,14 @@ class Window
 
     private InitCamera()
     {
-        this.camera.position.set(-32 * .3, 32 * .8, -32 * .3);
+        this.steve.getObject().then(object => {
+            this.camera.position.set(object.position.x + 1, object.position.y + 8, object.position.z);
+            this.camera.rotation.set(object.rotation.x, object.rotation.y, object.rotation.z);
+            const euler = new THREE.Euler(0, 0, 0, 'YXZ');
+            euler.x = object.rotation.x;
+            euler.y = object.rotation.y - 1.6;
+            this.camera.quaternion.setFromEuler(euler);
+        });
     }
 
     private UpdateViewPort()
@@ -73,6 +105,7 @@ class Window
     {
         window.addEventListener('resize', () => { this.windowNeedToResize = true });
         window.addEventListener('keydown', (e) => { this.onDocumentKeyDown(e) });
+        window.addEventListener("mousemove", (event) => {this.CameraMovement(event)});
     }
 
     public Update(): void
@@ -84,7 +117,6 @@ class Window
             this.UpdateViewPort();
         }
 
-        this.controllers.update();
         this.renderer.render(this.mainScene, this.camera);
     }
 
@@ -95,9 +127,6 @@ class Window
 
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         document.body.appendChild(this.renderer.domElement);
-
-        this.controllers.target.set(chunkSize / 2, chunkSize / 3, chunkSize / 2);
-        this.controllers.update();
 
         LightningUtilities.AddLight(this.mainScene, -1,  2,  4);
         LightningUtilities.AddLight(this.mainScene, 1, -1, -2);
